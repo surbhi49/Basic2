d05ba9077536a78a0e0b785088299a2922c51426
unknown <surbhi@49>
2021-09-16 13:19:58 +0530

    initial project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+
+/*
 // math object//
 console.log(Math.round(3.45689))
 
@@ -35,7 +37,7 @@ console.log(devision1==devision2)
 
 /*let nameone= "surbhi"
 let nametwo= "sakshi"
-console.log(nameone == nametwo)*/
+console.log(nameone == nametwo)
 
 let num1=[9,8,7]
 num1[0]=10
@@ -122,6 +124,102 @@ let profile5 ={
      salary:'15k'
      
 }
-console.log(profile5)
+console.log(profile5) */
 
-//array end//
+
+
+
+
+
+//Declare a variable named challenge and assign it to an initial value ' JavaScript'. 
+
+ let challenge='javascript'
+console.log(challenge)
+
+ //Print the string on the browser console using console.log() 
+ let Name='divya';
+ console.log(Name)
+ 
+
+ // Print the length of the string on the browser console using console.log() 
+
+ let str='subhashchandra';
+ str.length=13;
+ console.log('13')
+
+ //Make an object and fill your profile value like {name, age, father-name, etc} 
+
+ let profile ={
+    name:'surbhi',
+    age:20,
+    role:'student',
+    job:false,
+    salary:null
+
+}
+console.log(profile)
+
+//Make an array of object in which fill all class member value.
+
+let array = [
+   {
+    name:'Sakshi',
+    age:20,
+    role:'student',
+    job:false,
+    salary:null 
+ },
+ {
+    name:'surbhi',
+    age:20,
+    role:'student',
+    job:false,
+    salary:null
+ },
+ {
+   name:'Aayush',
+    age:30,
+   role:'Emolyee',
+   job:true,
+    salary:10000
+ },
+ {
+ name:'Husaain',
+ age:18,
+ role:'student',
+ job:false,
+ salary:null
+ }
+]
+console.log(array)
+
+/* Using console.log() print out the following statement: 
+o The quote 'There is no exercise better for the heart than reaching down and lifting people up.' 
+by John Holmes teaches us to help one another.
+ */
+let sentence ='There is no exercise better for the heart than reaching down and lifting people up by John Holmes teaches us to help one another'
+console.log(sentence)
+
+/*Using console.log() print out the following quote by Mother Teresa: 
+o "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the 
+same -- with charity you give love, so don't just give money but reach out your hand instead."  */
+let sente='Love is not patronizing and charity isnt about pity, it is about love. Charity and love are the same -- with charity you give love, so dont just give money but reach out your hand instead.'
+console.log(sente)
+
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal. 
+let num= '10'
+let nums='10'
+console.log(num==nums)
+
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10. 
+console.log(parseFloat('9.8')==10)
+console.log(parseFloat('9.8')+0.2==10)
+console.log(parseFloat('9.8')==parseFloat('9.8)'))
+
+/*
+//Access the 'JavaScript' string characters using a random number.
+
+let strng= Math.random().toString(10);
+console.log(string)
+*/
+
